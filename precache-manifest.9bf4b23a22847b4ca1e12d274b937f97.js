0ccdbeeab1f37173976faa68d0f1c59d9e08c131
nkriti92 <nkriti92@gmail.com>
2020-08-13 00:45:17 -0700

    Updates

---
+++ precache-manifest.9bf4b23a22847b4ca1e12d274b937f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b07ffb732112717d44fe0a6062c3bc4",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "657baa2f2bfb2055aa46",
+    "url": "/portfolio-2020/static/css/main.df24737b.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio-2020/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-2020/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657baa2f2bfb2055aa46",
+    "url": "/portfolio-2020/static/js/main.32420c8a.chunk.js"
+  },
+  {
+    "revision": "0f01e84f6a62408e758e",
+    "url": "/portfolio-2020/static/js/runtime-main.9007be83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
